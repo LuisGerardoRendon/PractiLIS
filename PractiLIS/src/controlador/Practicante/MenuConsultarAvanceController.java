d1f714a12d7ecbfc4d384be0ef500e04aa1a938b
LuisGerardoRendon <50636600+LuisGerardoRendon@users.noreply.github.com>
2020-07-07 18:38:45 -0500

    Primer Commit

---
+++ PractiLIS/src/controlador/Practicante/MenuConsultarAvanceController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.Practicante;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class MenuConsultarAvanceController implements Initializable {
+
+    @FXML
+    private Label labelProgreso;
+    @FXML
+    private Button botonRegresar;
+    @FXML
+    private Label labelNombreProyecto;
+    @FXML
+    private Label labelSetNombreProyecto;
+    @FXML
+    private Label labelHorasRegistradas;
+    @FXML
+    private Label labelSetHorasRegistradas;
+    @FXML
+    private Label labelTotalHoras;
+    @FXML
+    private Label label200;
+    @FXML
+    private TableView<?> tablaReportesEntregados;
+    @FXML
+    private TableColumn<?, ?> columnaNombreReporte;
+    @FXML
+    private TableColumn<?, ?> comlumnaFechaEntrega;
+    @FXML
+    private TableColumn<?, ?> ColumnaHorasCubiertas;
+    @FXML
+    private Label labelSetAvance;
+    @FXML
+    private Label labelAvance;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void regresar(ActionEvent event) {
+        System.out.println("NO SE GIT HUB");//GG
+    }
+    
+}
