fd47211da56cd8d969a3cc5012b42484822b8ab2
LuisGerardoRendon <50636600+LuisGerardoRendon@users.noreply.github.com>
2020-07-07 19:01:29 -0500

    ewewewew

--- PractiLIS/src/controlador/Practicante/MenuConsultarAvanceController.java
+++ PractiLIS/src/controlador/Practicante/MenuConsultarAvanceController.java
@@ -61,6 +61,7 @@ public class MenuConsultarAvanceController implements Initializable {
 
     @FXML
     private void regresar(ActionEvent event) {
+        System.out.println("OTRO COMENTARIO 7:01");
         System.out.println("Prueba 3");
         System.out.println("BY GERARDO");
         System.out.println("by pale");
